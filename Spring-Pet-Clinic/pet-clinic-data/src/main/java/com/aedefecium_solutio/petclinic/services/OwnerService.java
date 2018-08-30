a258fad5c69af1ba5dc6222103420040b628e9f1
darm191065 <cheznl191065@gmail.com>
2018-08-30 16:41:20 -0500

    MapServices

--- Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/aedefecium_solutio/petclinic/services/OwnerService.java
+++ Spring-Pet-Clinic/pet-clinic-data/src/main/java/com/aedefecium_solutio/petclinic/services/OwnerService.java
@@ -1,16 +1,8 @@
 package com.aedefecium_solutio.petclinic.services;
 
-import java.util.Set;
-
 import com.aedefecium_solutio.petclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
 	Owner findByLastName(String lastName);
-	
-	Owner findById(Long id);
-	
-	Owner save(Owner owner);
-	
-	Set<Owner> findAll();
 }
